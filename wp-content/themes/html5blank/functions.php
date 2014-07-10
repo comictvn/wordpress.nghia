14c35336216837ec26154de89a8d6ee799537478
comictvn <comictvn@gmail.com>
2014-07-10 16:05:45 +0700

    Update WP

--- wp-content/themes/html5blank/functions.php
+++ wp-content/themes/html5blank/functions.php
@@ -468,24 +468,34 @@ function stheme_product_post_type() {
         'not_found_in_trash'  => __( 'Không có sản phẩm trong này.', 'stheme' ),
     );
     $args = array(
-        'label'               => __( 'sproduct', 'stheme' ),
-        'description'         => __( 'Quản lý danh mục sản phẩm', 'stheme' ),
-        'labels'              => $labels,
-        'supports'            => array('title', 'editor', 'thumbnail', 'comments', 'excerpt'),
-        'taxonomies'          => array( 'category', 'post_tag' ),
-        'hierarchical'        => false,
-        'public'              => true,
-        'rewrite' => array("slug" => "san-pham"),
-        'show_ui'             => true,
-        'show_in_menu'        => true,
-        'show_in_nav_menus'   => true,
-        'show_in_admin_bar'   => true,
-        'menu_position'       => 5,
-        'can_export'          => true,
-        'has_archive'         => false,
-        'exclude_from_search' => false,
-        'publicly_queryable'  => true,
-        'capability_type'     => 'post'
+        'labels' => $labels, //Gọi các label trong biến $label ở trên
+        'description' => 'Post type đăng sản phẩm', //Mô tả của post type
+        'supports' => array(
+            'title',
+            'editor',
+	    'price',
+            'excerpt',
+            'author',
+            'thumbnail',
+            'comments',
+            'trackbacks',
+            'revisions',
+            'custom-fields'
+        ), //Các tính năng được hỗ trợ trong post type
+        'taxonomies' => array( 'category', 'post_tag' ), //Các taxonomy được phép sử dụng để phân loại nội dung
+        'hierarchical' => false, //Cho phép phân cấp, nếu là false thì post type này giống như Post, false thì giống như Page
+        'public' => true, //Kích hoạt post type
+        'show_ui' => true, //Hiển thị khung quản trị như Post/Page
+        'show_in_menu' => true, //Hiển thị trên Admin Menu (tay trái)
+        'show_in_nav_menus' => true, //Hiển thị trong Appearance -> Menus
+        'show_in_admin_bar' => true, //Hiển thị trên thanh Admin bar màu đen.
+        'menu_position' => 5, //Thứ tự vị trí hiển thị trong menu (tay trái)
+        'menu_icon' => '', //Đường dẫn tới icon sẽ hiển thị
+        'can_export' => true, //Có thể export nội dung bằng Tools -> Export
+        'has_archive' => true, //Cho phép lưu trữ (month, date, year)
+        'exclude_from_search' => false, //Loại bỏ khỏi kết quả tìm kiếm
+        'publicly_queryable' => true, //Hiển thị các tham số trong query, phải đặt true
+        'capability_type' => 'post' //
     );
     register_post_type( 'sproduct', $args );
  
--- wp-content/themes/html5blank/template-demo3.php
+++ wp-content/themes/html5blank/template-demo3.php
@@ -3,8 +3,27 @@
 	<main role="main">
 		<!-- section -->
 		<section>
+<?php
+	$query = new WP_Query( array(
+        'post_type' => 'contact',
+        'color' => 'blue',
+        'posts_per_page' => -1,
+        'order' => 'ASC',
+        'orderby' => 'title',
+    ) );
 
-			<h1><?php the_title(); ?></h1>
+
+?>
+<ul class="clothes-listing">
+            <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+            <li id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+		<?php echo get_post_meta( $post->ID, 'phone_number', true ); ?>
+            </li>
+            <?php endwhile;
+            wp_reset_postdata(); ?>
+        </ul>
+			<h1>a<?php the_title(); ?></h1>
 
 		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
